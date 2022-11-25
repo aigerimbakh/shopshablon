76842e8195e32f59cfdfd6bac810dec71a4851c0
aigerimbakh <milashkalovec@gmail.com>
2022-11-26 00:00:31 +0300

    git init

---
+++ src/main/java/kz/narxoz/shopshablon/ShopshablonApplication.java
@@ -0,0 +1,13 @@
+package kz.narxoz.shopshablon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopshablonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopshablonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kz/narxoz/shopshablon/ShopshablonApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.narxoz.shopshablon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopshablonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
